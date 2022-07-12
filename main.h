9522dd4f128bb64f0fb33130ea7bd548b26ea607
kevkiiru <kevkiiru6@gmail.com>
2022-07-13 02:29:32 +0300

    the main header file commit

--- main.h
+++ main.h
@@ -1,23 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-
 /**
- * struct print - definition structure 
- * @type_arg: type
- * @f: function to print
-*/
-
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
 typedef struct print
 {
 	char *type_arg;
 	int (*f)(va_list, char *, unsigned int);
 } print_t;
-
 
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
@@ -59,5 +59,5 @@ char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
 
-#endif
 
+#endif
