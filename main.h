a23bcb053da4214fd3485a5a9a14be4ad14a53cf
kevkiiru <kevkiiru6@gmail.com>
2022-07-13 03:34:15 +0300

    the main header file commit

--- main.h
+++ main.h
@@ -57,6 +57,7 @@ char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+int main(void);
 
 
 
